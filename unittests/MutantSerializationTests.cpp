eb4ddab0ab8ed7392890e76d9f88c8b81ed415fd
Yuta Saito <kateinoigakukun@gmail.com>
2021-02-26 18:59:47 +0900

    apply clang-format

--- unittests/MutantSerializationTests.cpp
+++ unittests/MutantSerializationTests.cpp
@@ -1,12 +1,12 @@
-#include "gtest/gtest.h"
 #include "MullXCTest/MutantSerialization.h"
-#include <mull/Diagnostics/Diagnostics.h>
-#include <mull/Mutators/CXX/ArithmeticMutators.h>
-#include <mull/MutationPoint.h>
-#include <mull/SourceLocation.h>
+#include "gtest/gtest.h"
 #include <llvm/ADT/ArrayRef.h>
-#include <llvm/Support/Program.h>
 #include <llvm/Support/FileUtilities.h>
+#include <llvm/Support/Program.h>
+#include <mull/Diagnostics/Diagnostics.h>
+#include <mull/MutationPoint.h>
+#include <mull/Mutators/CXX/ArithmeticMutators.h>
+#include <mull/SourceLocation.h>
 
 using namespace mull_xctest;
 using namespace mull;
@@ -15,21 +15,22 @@ using namespace llvm;
 const char fixture[] = "";
 
 TEST(MutantSerializationTest, singlePoint) {
-    Diagnostics diags;
-    MutatorsFactory factory(diags);
-    factory.init();
+  Diagnostics diags;
+  MutatorsFactory factory(diags);
+  factory.init();
 
-    cxx::AddToSub mutator;
-    SourceLocation loc("unit_dir", "unit_file_path", "dir", "file_path", 1, 2);
-    MutationPoint point(&mutator, "replacement", loc, "diagnostics");
-    std::string entriesString;
-    raw_string_ostream stream(entriesString);
-    MutantSerializer serializer(stream);
-    serializer.serialize(&point);
+  cxx::AddToSub mutator;
+  SourceLocation loc("unit_dir", "unit_file_path", "dir", "file_path", 1, 2);
+  MutationPoint point(&mutator, "replacement", loc, "diagnostics");
+  std::string entriesString;
+  raw_string_ostream stream(entriesString);
+  MutantSerializer serializer(stream);
+  serializer.serialize(&point);
 
-    MutantDeserializer deserializer(StringRef(entriesString), factory);
-    auto restored = deserializer.deserialize();
+  MutantDeserializer deserializer(StringRef(entriesString), factory);
+  auto restored = deserializer.deserialize();
 
-    ASSERT_EQ(point.getMutator()->mutatorKind(), restored->getMutator()->mutatorKind());
-    ASSERT_EQ(point.getUserIdentifier(), restored->getUserIdentifier());
+  ASSERT_EQ(point.getMutator()->mutatorKind(),
+            restored->getMutator()->mutatorKind());
+  ASSERT_EQ(point.getUserIdentifier(), restored->getUserIdentifier());
 }
