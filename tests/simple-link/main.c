13592e68e52b0e4da81989539f7818a5aa887fe1
Yuta Saito <kateinoigakukun@gmail.com>
2021-02-27 13:47:33 +0900

    fix simple-link

--- tests/simple-link/main.c
+++ tests/simple-link/main.c
@@ -1,6 +1,6 @@
 // RUN: clang -c -fembed-bitcode -g %S/../Inputs/add.c -o %t-add.o
 // RUN: clang -c -fembed-bitcode -g %s -o %t.o
-// RUN: MULL_XCTEST_LINKER=clang mull-ld %t-add.o %t.o -o %t.out
+// RUN: MULL_XCTEST_ARGS="--linker clang" mull-ld %t-add.o %t.o -o %t.out
 
 int add(int a, int b);
 
