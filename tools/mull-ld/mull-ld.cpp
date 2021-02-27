5d64246927da87d696ae478881256b41047e97dd
Yuta Saito <kateinoigakukun@gmail.com>
2021-02-27 13:29:17 +0900

    Parse mull-ld args from env variable

--- tools/mull-ld/mull-ld.cpp
+++ tools/mull-ld/mull-ld.cpp
@@ -7,6 +7,10 @@
 #include <mull/Config/Configuration.h>
 #include <mull/Diagnostics/Diagnostics.h>
 #include <mull/Filters/Filters.h>
+#include <mull/Filters/Filter.h>
+#include <mull/Filters/JunkMutationFilter.h>
+#include <mull/Filters/NoDebugInfoFilter.h>
+#include <mull/Filters/FilePathFilter.h>
 #include <mull/MutationsFinder.h>
 #include <mull/Mutators/MutatorsFactory.h>
 #include <mull/Parallelization/TaskExecutor.h>
@@ -14,6 +18,24 @@
 #include <string>
 #include <unistd.h>
 #include <vector>
+
+using namespace llvm::cl;
+
+OptionCategory MullLDCategory("mull-ld");
+
+opt<std::string> Linker(
+    "linker",
+    desc("Linker program"),
+    value_desc("string"),
+    Optional,
+    cat(MullLDCategory));
+
+opt<bool> DebugEnabled(
+  "debug",
+  desc("Enables Debug Mode: more logs are printed"),
+  Optional,
+  init(false),
+  cat(MullLDCategory));
 
 void extractBitcodeFiles(std::vector<std::string> &args,
                          std::vector<llvm::StringRef> &bitcodeFiles) {
@@ -37,14 +59,14 @@ static void validateInputFiles(const std::vector<llvm::StringRef> &inputFiles) {
 static void validateConfiguration(const mull::Configuration &configuration,
                                   mull::Diagnostics &diags) {
   if (configuration.linker.empty()) {
-    diags.error("No linker specified. Please set MULL_XCTEST_LINKER "
+    diags.error("No linker specified. Please set --linker option in MULL_XCTEST_ARGS"
                 "environment variable.");
   }
 }
 
 llvm::Optional<std::string> getLinkerPath(mull::Diagnostics &diagnositcs) {
-  if (const auto env = getenv("MULL_XCTEST_LINKER")) {
-    return std::string(env);
+  if (!Linker.empty()) {
+    return std::string(Linker);
   }
   mull::Runner runner(diagnositcs);
   auto result =
@@ -60,10 +82,48 @@ llvm::Optional<std::string> getLinkerPath(mull::Diagnostics &diagnositcs) {
   return resultOutput;
 }
 
+void bootstrapFilters(mull::Filters &filters,
+                      std::vector<std::unique_ptr<mull::Filter>> &filterStorage) {
+  auto *noDebugInfoFilter = new mull::NoDebugInfoFilter;
+  auto *filePathFilter = new mull::FilePathFilter;
+  filterStorage.emplace_back(noDebugInfoFilter);
+  filterStorage.emplace_back(filePathFilter);
+
+  filters.mutationFilters.push_back(noDebugInfoFilter);
+  filters.functionFilters.push_back(noDebugInfoFilter);
+  filters.instructionFilters.push_back(noDebugInfoFilter);
+
+  filters.mutationFilters.push_back(filePathFilter);
+  filters.functionFilters.push_back(filePathFilter);
+
+}
+
+void bootstrapConfiguration(mull::Configuration &configuration,
+                            mull::Diagnostics &diagnostics) {
+  if (const auto linker = getLinkerPath(diagnostics)) {
+    configuration.linker = linker.getValue();
+  } else {
+    diagnostics.error("no real linker found");
+  }
+  configuration.debugEnabled = DebugEnabled;
+  configuration.linkerTimeout = mull::MullDefaultLinkerTimeoutMilliseconds;
+  configuration.timeout = mull::MullDefaultTimeoutMilliseconds;
+}
+
 int main(int argc, char **argv) {
   mull::Diagnostics diagnostics;
   std::vector<llvm::StringRef> inputObjects;
   std::vector<std::unique_ptr<llvm::MemoryBuffer>> bitcodeBuffers;
+
+  bool validOptions = llvm::cl::ParseCommandLineOptions(
+    1, argv, "", &llvm::errs(), "MULL_XCTEST_ARGS");
+  if (!validOptions) {
+    return 1;
+  }
+
+  if (DebugEnabled) {
+    diagnostics.enableDebugMode();
+  }
 
   std::vector<std::string> args(argv + 1, argv + argc);
   extractBitcodeFiles(args, inputObjects);
@@ -73,21 +133,15 @@ int main(int argc, char **argv) {
   llvm::InitializeNativeTargetAsmPrinter();
   llvm::InitializeNativeTargetAsmParser();
 
+  std::vector<std::unique_ptr<mull::Filter>> filterStorage;
   mull::Filters filters;
   mull::Configuration configuration;
-  // FIXME: Link input objects with real linker
-  configuration.executable = "echo";
-  if (const auto linker = getLinkerPath(diagnostics)) {
-    configuration.linker = linker.getValue();
-  } else {
-    diagnostics.error("no real linker found");
-  }
-  configuration.debugEnabled = true;
-  configuration.linkerTimeout = mull::MullDefaultLinkerTimeoutMilliseconds;
-  configuration.timeout = mull::MullDefaultTimeoutMilliseconds;
-  diagnostics.enableDebugMode();
+
+  bootstrapConfiguration(configuration, diagnostics);
 
   validateConfiguration(configuration, diagnostics);
+
+  bootstrapFilters(filters, filterStorage);
   mull::MutatorsFactory factory(diagnostics);
   mull::MutationsFinder mutationsFinder(factory.mutators({"cxx_comparison"}),
                                         configuration);
