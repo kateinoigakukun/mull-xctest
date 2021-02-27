bcdcb9b71c996cce49f2a256dac97a656103a408
Yuta Saito <kateinoigakukun@gmail.com>
2021-02-27 13:40:34 +0900

    add failing lit test

--- tests/swift-filters/try.swift
+++ tests/swift-filters/try.swift
@@ -1,9 +1,11 @@
 // RUN: %empty-directory(%t)
 // RUN: swiftc %s -tools-directory %mull-xctest-bin -embed-bitcode -g -o %t/try.swift.out -sdk %target-sdk
 // RUN: mull-dump-mutants %t/try.swift.out | FileCheck %s
+// cxx_ne_to_eq produced by `try` is not filtered yet
+// XFAIL: *
 
 // CHECK-NOT: cxx_eq_to_ne /<compiler-generated>:0:0
-// CHECK-NOT: cxx_ne_to_eq {{.*}}try.swift:11:9
+// CHECK-NOT: cxx_ne_to_eq {{.+}}try.swift:17:9
 
 struct MyError: Error {}
 
