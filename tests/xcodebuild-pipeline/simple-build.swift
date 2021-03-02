451900f180522b55c80b971f315ccc94c530d2aa
Yuta Saito <kateinoigakukun@gmail.com>
2021-03-03 00:56:53 +0900

    improve xcodebuild test

--- tests/xcodebuild-pipeline/simple-build.swift
+++ tests/xcodebuild-pipeline/simple-build.swift
@@ -1,2 +1,4 @@
+// RUN: %empty-directory(%t)
 // RUN: export MULL_XCTEST_ARGS="--linker clang --linker-flavor=clang"
-// RUN: xcodebuild -project %S/../Inputs/Project/Project.xcodeproj -scheme DynamicFramework -destination "%iossim-dest" build-for-testing %xcodebuild-opts
+// RUN: xcodebuild -project %S/../Inputs/Project/Project.xcodeproj -scheme DynamicFramework -destination "%iossim-dest" -derivedDataPath %t build-for-testing %xcodebuild-opts
+// RUN: xcodebuild test-without-building -destination "%iossim-dest" -xctestrun %t/Build//Products/DynamicFramework_iphonesimulator14.4-x86_64.xctestrun
