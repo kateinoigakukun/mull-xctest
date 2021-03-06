5b87ecb350dde4446def96c938991eb3aef42fab
Yuta Saito <kateinoigakukun@gmail.com>
2021-03-06 19:30:45 +0900

    add install command

---
+++ cmake/modules/InstallSymlink.cmake
@@ -0,0 +1,17 @@
+function(install_symlink name target outdir)
+  set(DESTDIR $ENV{DESTDIR})
+  if(CMAKE_HOST_UNIX)
+    set(LINK_OR_COPY create_symlink)
+  else()
+    set(LINK_OR_COPY copy)
+  endif()
+
+  set(bindir "${DESTDIR}${CMAKE_INSTALL_PREFIX}/${outdir}/")
+
+  message(STATUS "Creating ${name} to ${bindir}")
+
+  execute_process(
+    COMMAND "${CMAKE_COMMAND}" -E ${LINK_OR_COPY} "${target}" "${name}"
+    WORKING_DIRECTORY "${bindir}")
+
+endfunction()
