5f5a3f0a0f38d7e7babbb89da2e970a9b78119fa
Yuta Saito <kateinoigakukun@gmail.com>
2021-03-11 19:41:11 +0900

    fix broken test case

--- tests/xcodebuild-pipeline/simple-build.swift
+++ tests/xcodebuild-pipeline/simple-build.swift
@@ -4,9 +4,9 @@
 // RUN: xcodebuild test-without-building -destination "%iossim-dest" -xctestrun %t/Build/Products/DynamicFramework_iphonesimulator14.4-x86_64.xctestrun
 // RUN: mull-dump-mutants %t/Build/Products/Debug-iphonesimulator/DynamicFramework.framework/DynamicFramework | FileCheck %s --check-prefix=DUMP-CHECK
 
-// DUMP-CHECK-DAG: Mutation Point: cxx_gt_to_ge {{.*}}/Library.swift:2:16
-// DUMP-CHECK-DAG: Mutation Point: cxx_gt_to_le {{.*}}/Library.swift:2:16
+// DUMP-CHECK-DAG: Mutation Point: cxx_gt_to_ge {{.*}}/Library.swift:3:16
+// DUMP-CHECK-DAG: Mutation Point: cxx_gt_to_le {{.*}}/Library.swift:3:16
 
 
 // RUN: mull-xctestrun --test-target DynamicFrameworkTests -Xxcodebuild -destination -Xxcodebuild "platform=iOS Simulator,name=iPhone 8" %t/Build/Products/DynamicFramework_iphonesimulator14.4-x86_64.xctestrun | FileCheck %s --check-prefix=RUN-CHECK
-// RUN-CHECK: Library.swift:2:16: warning: Survived: Replaced > with >= [cxx_gt_to_ge]
+// RUN-CHECK: Library.swift:3:16: warning: Survived: Replaced > with >= [cxx_gt_to_ge]
