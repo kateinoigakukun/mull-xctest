cd7e3dcd9177f634627ad9172f064bed8aaf4cad
Yuta Saito <kateinoigakukun@gmail.com>
2021-03-15 18:47:27 +0900

    fix filecheck directive

--- tests/xcodebuild-pipeline/multi-targets.swift
+++ tests/xcodebuild-pipeline/multi-targets.swift
@@ -4,7 +4,7 @@
 // RUN: xcodebuild test-without-building -destination "%iossim-dest" -xctestrun %t/Build/Products/MultiTargets_iphonesimulator14.4-x86_64.xctestrun
 
 // RUN: mull-xctestrun -Xxcodebuild -destination -Xxcodebuild "platform=iOS Simulator,name=iPhone 8" %t/Build/Products/MultiTargets_iphonesimulator14.4-x86_64.xctestrun | FileCheck %s --check-prefix=RUN-CHECK
-// RUN-CHECK: DynamicFramework/Library.swift:3:16: warning: Survived: Replaced > with >= [cxx_gt_to_ge]
-// RUN-CHECK: DynamicFramework/Library.swift:3:16: warning: Survived: Replaced > with <= [cxx_gt_to_le]
-// RUN-CHECK: StaticFramework/StaticFramework.swift:2:16: warning: Survived: Replaced > with >= [cxx_gt_to_ge]
-// RUN-CHECK: StaticFramework/StaticFramework.swift:2:16: warning: Survived: Replaced > with <= [cxx_gt_to_le]
+// RUN-CHECK-DAG: DynamicFramework/Library.swift:3:16: warning: Survived: Replaced > with >= [cxx_gt_to_ge]
+// RUN-CHECK-DAG: DynamicFramework/Library.swift:3:16: warning: Survived: Replaced > with <= [cxx_gt_to_le]
+// RUN-CHECK-DAG: StaticFramework/StaticFramework.swift:2:16: warning: Survived: Replaced > with >= [cxx_gt_to_ge]
+// RUN-CHECK-DAG: StaticFramework/StaticFramework.swift:2:16: warning: Survived: Replaced > with <= [cxx_gt_to_le]
