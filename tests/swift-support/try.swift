2666b8f56ae6bcf94c8742090676368e69983438
Yuta Saito <kateinoigakukun@gmail.com>
2021-05-10 19:09:57 +0900

    Relax test case checking item

--- tests/swift-support/try.swift
+++ tests/swift-support/try.swift
@@ -2,7 +2,6 @@
 // RUN: swiftc %s -tools-directory %mull-xctest-bin -embed-bitcode -g -o %t/try.swift.out -sdk %target-sdk
 // no mutation
 // RUN: mull-dump-mutants %t/try.swift.out | FileCheck %s
-// CHECK: no mutant info found
 // CHECK-NOT: cxx_eq_to_ne /<compiler-generated>:0:0
 
 struct MyError: Error {}
