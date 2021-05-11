3ddebb89ae1ec9afe55ada0faac8ca62fc6aedbb
Yuta Saito <kateinoigakukun@gmail.com>
2021-05-11 10:04:56 +0900

    Split test case for equal comparison

---
+++ tests/swift-support/comparison_eq.swift
@@ -0,0 +1,26 @@
+// RUN: %empty-directory(%t)
+// RUN: swiftc %s -tools-directory %mull-xctest-bin -embed-bitcode -g -o %t/comp.swift.out -sdk %target-sdk
+// RUN: mull-dump-mutants %t/comp.swift.out | FileCheck %s
+
+func equal(_ lhs: Int, _ rhs: Int) -> Bool {
+    return lhs == rhs
+}
+
+func not_equal(_ lhs: Int, _ rhs: Int) -> Bool {
+    return lhs != rhs
+}
+
+// CHECK-DAG: Mutation Point: cxx_eq_to_ne {{.*}}/comparison_eq.swift:6:16
+// CHECK-DAG: Mutation Point: cxx_ne_to_eq {{.*}}/comparison_eq.swift:10:16
+
+// // FAIL-RUN: env TEST_cxx_eq_to_ne=1 "cxx_eq_to_ne:%s:6:16"=1 %t/comp.swift.out
+// if getenv("TEST_cxx_eq_to_ne") != nil {
+//     assert(equal(1, 1) == false)
+//     assert(equal(1, 2) == true)
+// }
+// 
+// // FAIL-RUN: env TEST_cxx_ne_to_eq=1 "cxx_ne_to_eq:%s:10:16"=1 %t/comp.swift.out
+// if getenv("TEST_cxx_ne_to_eq") != nil {
+//     assert(not_equal(1, 1) == true)
+//     assert(not_equal(1, 2) == false)
+// }
